f4cde5f09e61352f3529f95089bf245ae698e964
Jatin patel <Jatinpatel@jatins-MacBook-Pro.local>
2015-11-01 19:20:05 +0530

    first commit

---
+++ Koob/MJPullToRefreshLoader.swift
@@ -0,0 +1,21 @@
+//
+//  MJPullToRefreshLoader.swift
+//  Koob
+//
+//  Created by Jatin patel on 11/1/15.
+//  Copyright © 2015 Jatin patel. All rights reserved.
+//
+
+import UIKit
+
+class MJPullToRefreshLoader: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Koob/MJRefreshControl.swift
@@ -0,0 +1,21 @@
+//
+//  MJRefreshControl.swift
+//  Koob
+//
+//  Created by Jatin patel on 10/31/15.
+//  Copyright © 2015 Jatin patel. All rights reserved.
+//
+
+import UIKit
+
+class MJRefreshControl: UIRefreshControl {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
